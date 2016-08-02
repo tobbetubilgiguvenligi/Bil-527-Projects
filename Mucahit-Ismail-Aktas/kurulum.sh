df1430ff467de7b16d579423e43423126ec482db
mucahit-ismail-aktas <mucahitismailaktas@gmail.com>
2016-08-02 05:54:31 +0300

    BroIDS Kurulum ve Kullanım Klavuzu

--- Mucahit-Ismail-Aktas/kurulum.sh
+++ Mucahit-Ismail-Aktas/kurulum.sh
@@ -1,7 +1,5 @@
 sudo apt-get update && apt-get upgrade
 sudo apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev
-mkdir ids
-cd ids
 git clone --recursive git://git.bro.org/bro
 cd bro
 ./configure && make && make install
